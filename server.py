8863362158c6143ed88f2e0944b7ecb5e37c4b5f
mohan <mohanthosare@gmail.com>
2023-07-29 10:47:30 +0530

    exam

--- server.py
+++ server.py
@@ -2,10 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/",methods=["GET"])
+@app.route("/", methods=["GET"])
 def root():
-     return "<h1>welcome to ITIL exam</h1>"
-     return "<h1>PRN number = 230344223028</h1>"
-     return "<h1>Name = Mohan Thosare</h1>"
-     return "<h1>phone number = 8855989893</h1>"
-app.run(host="0.0.0.0", port=4000, debug=True)
+    return """
+    <h1>Welcome to ITIL exam</h1>
+    <p>PRN number = 230344223028</p>
+    <p>Name = Mohan Thosare</p>
+    <p>Phone number = 8855989893</p>
+    """
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=4000, debug=True)
